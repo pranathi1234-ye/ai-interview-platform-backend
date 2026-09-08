accb8336957ca5414276d6841d7d4a5bfe5f1b64
pranathi1234-ye <yeruvapranathi2007@gmail.com>
2026-09-08 21:26:56 +0530

    Fix backend User entity

--- src/main/java/com/pranathi/ai_interview_backend/AiInterviewBackendApplication.java
+++ src/main/java/com/pranathi/ai_interview_backend/AiInterviewBackendApplication.java
@@ -2,12 +2,15 @@ package com.pranathi.ai_interview_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EntityScan(basePackages = "com.pranathi.entity")
+@EnableJpaRepositories(basePackages = "com.pranathi.repository")
 public class AiInterviewBackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AiInterviewBackendApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(AiInterviewBackendApplication.class, args);
+    }
 }
